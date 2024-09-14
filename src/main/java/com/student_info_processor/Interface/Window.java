659f65fff9bd0a7115f3b749b1443c9fed84890f
Gao Shenghanfe <gsh1130@sjtu.edu.cn>
2024-09-14 15:16:28 +0800

    feat: window test

--- src/main/java/com/student_info_processor/Interface/Window.java
+++ src/main/java/com/student_info_processor/Interface/Window.java
@@ -1,9 +1,51 @@
 package com.student_info_processor.Interface;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
 public class Window {
+    // fields
+    private JFrame frame;
+
+    // constructor
+
+    public Window(String title) {
+        System.out.println("Window of title " + title);
+        frame = new JFrame(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+
+    public Window(int a, int b, String title) {
+        System.out.println("Window of title " + title + "and size " + a + " x " + b);
+        frame = new JFrame(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(a, b);
+        frame.setVisible(true);
+    }
+
+    // methods
+
     public int windowOut() {
         System.out.println("WindowOut");
         return 0;
     }
-    
+
+    public void windowClose() {
+        System.out.println("WindowClose of title " + frame.getTitle());
+        frame.dispose();
+    }
+
+    public void window_invisble() {
+        System.out.println("WindowInvisible of title " + frame.getTitle());
+        frame.setVisible(false);
+    }
+
+    public void window_visible() {
+        System.out.println("WindowVisible of title " + frame.getTitle());
+        frame.setVisible(true);
+    }
+
 }
--- src/main/java/com/student_info_processor/MyApp.java
+++ src/main/java/com/student_info_processor/MyApp.java
@@ -4,7 +4,13 @@ import com.student_info_processor.Interface.Window;
 
 public class MyApp {
     public static void main(String[] args) {
-        Window window = new Window();
+        Window window = new Window(1000,1000,"wind");
         window.windowOut();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        window.windowClose();
     }
 }
